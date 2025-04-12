aa95420295716402a478ee3e4c27492538e3f03a
Diego Torres <149275644+diegotorresg93@users.noreply.github.com>
2025-04-12 11:20:15 -0500

    variables

---
+++ variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    $cadena = "Hola mundo";
+    $anios12 = 12;
+
+    print $cadena;
+    echo $anios12;
+    
+    ?>
+</body>
+</html>
