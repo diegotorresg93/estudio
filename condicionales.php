646a6d6e8ae090b4c555c3abd4834cfd340938f4
Diego Torres <149275644+diegotorresg93@users.noreply.github.com>
2025-04-12 11:30:49 -0500

    condicionales

--- condicionales.php
+++ condicionales.php
@@ -3,17 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Variables en PHP</title>
+    <title>Condicionales en PHP</title>
 </head>
 <body>
-    <?php
     
-    $cadena = "<p>Hola mundo</p>";
-    $anios12 = 12;
-
-    print $cadena;
-    echo  $anios12;
-    
-    ?>
 </body>
 </html>
