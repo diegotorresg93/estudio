8a1336bcf6247522c799aabfec523dbcbedff650
Diego Torres <diegotorresg93@gmail.com>
2025-06-10 21:19:34 -0500

    Variables y tipos

--- variables.php
+++ variables.php
@@ -7,9 +7,18 @@
 </head>
 <body>
     <?php
-    
+    //Cadena
     $cadena = "<p>Hola mundo</p>";
     $anios12 = 12;
+    $nombre = "Diego";
+    $apellido = "Torres";
+    // Cadenas enteras integer
+    $edad = 12;
+    $deuda = -150000;
+    // Dobles o decimales
+    $ingreso = 1000.50;
+    // Variables booleanas
+    $casado = true;
 
     print $cadena;
     echo  $anios12;
