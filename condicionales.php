e1ebaf99ec911c081c1901407df638f54067cb76
Diego Torres <diegotorresg93@gmail.com>
2025-06-18 20:28:33 -0500

    Operadores de comparacion

--- condicionales.php
+++ condicionales.php
@@ -13,10 +13,30 @@
 //Mayor o igual >=
 //Igualdad ==
 $edad = "18";
-if ($edad == 18);
-//Igualdad extricta ===
+if ($edad == 18) {
+print "<p>Tienes dieciocho años</p>";
 
-   
+}
+//Igualdad extricta ===
+$edad = "18";
+if ($edad === 18) {
+print "Tienes dieciocho años";
+
+}else{
+   print "<p>Posiblemente tienes dieciocho, pero debe ser valor numérico</p>";
+}
+
+//Diferencia
+$edad = "18";
+if ($edad != 18) {
+print "No T
+tienes dieciocho años";
+
+}else{
+   print "<p>Si tienes 18 años, pasa por tu credencial para votar.</p>";
+}
+
+
    
    ?> 
 </body>
