9fcf001a0c454472c974d8e37bb94fbb476644d4
Diego Torres <diegotorresg93@gmail.com>
2025-07-15 20:50:20 -0500

    Forma

---
+++ valida.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valida</title>
+</head>
+<body>
+<?php
+
+$nombre = $_GET["nombre"];
+print "<p>Bienvenido, $nombre, a nuestra página</p>";
+
+
+
+?>    
+</body>
+</html>
