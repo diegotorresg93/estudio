784eecfcf7b62c50c232bc66709fe72cacbb9264
Diego Torres <diegotorresg93@gmail.com>
2025-08-11 20:24:59 -0500

    error forma

---
+++ error_forma.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+<p>ERROR EN LA CAPTURA DE DATOS</p>
+<p><a href="forma2.html">Regresar</a></p>
+</body>
+</html>
